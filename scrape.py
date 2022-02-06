0f67684a51d68c4b77addd91bbf68a99150d546a
Urvish Thakker <91555752+urvishthakker@users.noreply.github.com>
2022-02-06 10:48:15 -0500

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 10:43:57 2022
+
+@author: uthak
+"""
+
+from selectorlib import Extractor
+import requests 
+from time import sleep
+import csv
+
+# Create an Extractor by reading from the YAML file
+e = Extractor.from_yaml_file('booking.yml')
+
+def scrape(url):    
+    headers = {
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'DNT': '1',
+        'Upgrade-Insecure-Requests': '1',
+        # You may want to change the user agent if you get blocked
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+        'Referer': 'https://www.booking.com/index.en-gb.html',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    # Download the page using requests
+    print("Downloading %s"%url)
+    r = requests.get(url, headers=headers)
+    # Pass the HTML of the page and create 
+    return e.extract(r.text,base_url=url)
+
+# product_data = []
+with open("urls.txt",'r') as urllist, open('data.csv','w') as outfile:
+    fieldnames = [
+        "name",
+        "location",
+        "price",
+        "price_for",
+        "room_type",
+        "beds",
+        "rating",
+        "rating_title",
+        "number_of_ratings",
+        "url"
+    ]
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+    for url in urllist.readlines():
+        data = scrape(url) 
+        if data:
+            for h in data['hotels']:
+                writer.writerow(h)
+            # sleep(5)
+    
